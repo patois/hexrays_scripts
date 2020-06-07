4797e89aae49a283ce2522ab784b29659911173d
Dennis Elser <patois@users.noreply.github.com>
2020-06-07 21:13:38 +0200

    make cfunc available

--- find_expr/find_expr.py
+++ find_expr/find_expr.py
@@ -4,14 +4,16 @@ __author__ = "https://github.com/patois"
 
 def find_expr(ea, expr, parents=False):
     class expr_finder_t(ctree_visitor_t):
-        def __init__(self, expr, parents):
+        def __init__(self, cfunc, expr, parents):
             ctree_visitor_t.__init__(self,
                 CV_PARENTS if parents else CV_FAST)
+            self.cfunc = cfunc
             self.expr = expr
             self.found = []
             return
 
         def visit_expr(self, e):
+            cfunc = self.cfunc
             if eval(self.expr, globals(), locals()):
                 self.found.append(e)
             return 0
@@ -23,6 +25,6 @@ def find_expr(ea, expr, parents=False):
         return []
 
     expr = expr.replace("\n", "")
-    ef = expr_finder_t(expr, parents)
+    ef = expr_finder_t(cfunc, expr, parents)
     ef.apply_to_exprs(cfunc.body, None)
     return ef.found
