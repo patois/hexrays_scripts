9a33cbd8b78c2cb876d1c79cf6f90e28d6b5b1b0
Dennis Elser <patois@users.noreply.github.com>
2020-06-07 17:57:08 +0200

    Add files via upload

---
+++ find_expr/find_expr.py
@@ -0,0 +1,28 @@
+from idaapi import *
+
+__author__ = "https://github.com/patois"
+
+def find_expr(ea, expr, parents=False):
+    class expr_finder_t(ctree_visitor_t):
+        def __init__(self, expr, parents):
+            ctree_visitor_t.__init__(self,
+                CV_PARENTS if parents else CV_FAST)
+            self.expr = expr
+            self.found = []
+            return
+
+        def visit_expr(self, e):
+            if eval(self.expr, globals(), locals()):
+                self.found.append(e)
+            return 0
+
+    try:
+        cfunc = decompile(ea)
+    except:
+        print("%x: unable to decompile." % ea)
+        return []
+
+    expr = expr.replace("\n", "")
+    ef = expr_finder_t(expr, parents)
+    ef.apply_to_exprs(cfunc.body, None)
+    return ef.found
