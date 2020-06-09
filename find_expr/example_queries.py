7e081f5c74a74a5a0a14cda38096ff9b11014b6d
Dennis Elser <patois@users.noreply.github.com>
2020-06-09 16:22:52 +0200

    Add files via upload

--- find_expr/example_queries.py
+++ find_expr/example_queries.py
@@ -1,18 +1,25 @@
-from hr_toolbox import find_expr
+import hr_toolbox as tb
 from idaapi import *
 
 # ----------------------------------------------------------------------------
 def find_calls():
-    """find function calls"""
+    """find function calls
+    example function to be passed to hr_toolbox.display()
+
+    """
+
     query = lambda cf, e: (e.op is cot_call and
         e.x.op is cot_obj)
 
-    return _db_exec_query(query)
+    return tb.db_exec_query(query)
 
 # ----------------------------------------------------------------------------
 def find_memcpy():
-    """find calls to memcpy() where the 'n' argument
-    is signed"""
+    """find calls to memcpy() where the 'n' argument is signed
+    example function to be passed to hr_toolbox.display()
+
+    """
+
     query = lambda cf, e: (e.op is cot_call and
         e.x.op is cot_obj and
         'memcpy' in get_name(e.x.obj_ea) and
@@ -20,12 +27,15 @@ def find_memcpy():
         e.a[2].op is cot_var and
         cf.lvars[e.a[2].v.idx].tif.is_signed())
 
-    return _db_exec_query(query)
+    return tb.db_exec_query(query)
 
 # ----------------------------------------------------------------------------
 def find_sprintf():
-    """find calls to sprintf() where the format string
-    argument contains '%s'"""
+    """find calls to sprintf() where the format string argument contains '%s'
+    example function to be passed to hr_toolbox.display()
+
+    """
+
     query = lambda cfunc, e: (e.op is cot_call and
         e.x.op is cot_obj and
         'sprintf' in get_name(e.x.obj_ea) and
@@ -34,11 +44,15 @@ def find_sprintf():
         is_strlit(get_flags(e.a[1].obj_ea)) and
         b'%s' in get_strlit_contents(e.a[1].obj_ea, -1, 0, STRCONV_ESCAPE))
 
-    return _db_exec_query(query)
+    return tb.db_exec_query(query)
 
 # ----------------------------------------------------------------------------
 def find_gpa():
-    """find dynamically imported functions (Windows)"""
+    """find dynamically imported functions (Windows)
+    example function to be passed to hr_toolbox.display()
+
+    """
+
     query = lambda cfunc, e: (e.op is cot_call and
         e.x.op is cot_obj and
         'GetProcAddress' in get_name(e.x.obj_ea) and
@@ -48,72 +62,26 @@ def find_gpa():
 
     gpa = get_name_ea_simple('GetProcAddress')
     ea_list = [f.start_ea for f in [get_func(xref.frm) for xref in XrefsTo(gpa, XREF_FAR)] if f]
-    return _exec_query(query, list(dict.fromkeys(ea_list)))
+    return tb.exec_query(query, list(dict.fromkeys(ea_list)))
 
 # ----------------------------------------------------------------------------
-def _db_exec_query(query):
-    """run query on all functions in current db"""
-    result = []
-    for ea in Functions():
-        result += [e for e in find_expr(ea, query)]
-    return result
+def menu():
+    print("""Example commands:
 
-# ----------------------------------------------------------------------------
-def _exec_query(query, ea_list):
-    """run query on list of addresses"""
-    result = []
-    for ea in ea_list:
-        result += [e for e in find_expr(ea, query)]
-    return result
+    menu()
 
-# ----------------------------------------------------------------------------
-def qdb(query, fmt=lambda x:"%x: %s" % (x.ea, tag_remove(x.print1(None)))):
-    r = _db_exec_query(query)
-    try:
-        for e in r:
-            print(fmt(e))
-    except:
-        print("<qdb> error!")
-    return
+    d(find_memcpy)
+    d(find_sprintf)
+    da(find_gpa, 1)
 
-# ----------------------------------------------------------------------------
-def q(query, ea_list=[here()], fmt=lambda x:"%x: %s" % (x.ea, tag_remove(x.print1(None)))):
-    r = _exec_query(query, ea_list)
-    try:
-        for e in r:
-            print(fmt(e))
-    except:
-        print("<q> error!")
-    return
-
-# ----------------------------------------------------------------------------
-def d(f, fmt=lambda x:"%x: %s" % (x.ea, tag_remove(x.print1(None)))):
-    """execute f and display results according to fmt.
-    f is supposed to return a list of cexpr_t objects"""
-    try:
-        for e in f():
-            print(fmt(e))
-    except:
-        print("<d> error!")
-    return
-
-# ----------------------------------------------------------------------------
-def ds(f):
-    try:
-        d(f, lambda x:"%x: %s" % (x.ea,
-                get_strlit_contents(x.a[1].obj_ea, -1, 0, STRCONV_ESCAPE).decode("utf-8")))
-    except:
-        print("<ds> error!")
-    return
-
-# ----------------------------------------------------------------------------
-def m():
-    print("""Try running the following Python statements:
-
-d(find_memcpy)
-d(find_sprintf)
-ds(find_gpa)""")
+    qdb(lambda cf, e: e.op is cot_call)
+    q(lambda cf, e: e.op is cot_call, [here()], lambda e: "%s" % get_name(e.x.obj_ea)[::-1])
+    """)
     return
 
 if __name__ == "__main__":
-    m()
+    from hr_toolbox import display as d
+    from hr_toolbox import display_argstr as da
+    from hr_toolbox import query as q
+    from hr_toolbox import query_db as qdb
+    menu()
--- find_expr/hr_toolbox.py
+++ find_expr/hr_toolbox.py
@@ -1,8 +1,12 @@
 import ida_hexrays as hr
+import ida_bytes, idautils, ida_kernwin, ida_lines
 
 __author__ = "https://github.com/patois"
 
+# ----------------------------------------------------------------------------
 def find_expr(ea, expr, findall=True, parents=False):
+    """find expr within AST of decompiled function"""
+
     class expr_finder_t(hr.ctree_visitor_t):
         def __init__(self, cfunc, expr, parents):
             hr.ctree_visitor_t.__init__(self,
@@ -31,3 +35,90 @@ def find_expr(ea, expr, findall=True, parents=False):
     ef = expr_finder_t(cfunc, expr, parents)
     ef.apply_to_exprs(cfunc.body, None)
     return ef.found
+
+# ----------------------------------------------------------------------------
+def db_exec_query(query):
+    """run query on all functions in current db
+
+    returns list of cexpr_t
+    """
+
+    result = []
+    for ea in idautils.Functions():
+        result += [e for e in find_expr(ea, query)]
+    return result
+
+# ----------------------------------------------------------------------------
+def exec_query(query, ea_list):
+    """run query on list of addresses
+
+    returns list of cexpr_t
+    """
+
+    result = []
+    for ea in ea_list:
+        result += [e for e in find_expr(ea, query)]
+    return result
+
+# ----------------------------------------------------------------------------
+def query_db(query,
+        fmt=lambda x:"%x: %s" % (x.ea,
+            ida_lines.tag_remove(x.print1(None)))):
+    """run query on idb, print results"""
+
+    r = db_exec_query(query)
+    try:
+        for e in r:
+            print(fmt(e))
+    except:
+        print("<query_db> error!")
+    return
+
+# ----------------------------------------------------------------------------
+def query(query,
+        ea_list=None,
+        fmt=lambda x:"%x: %s" % (x.ea,
+            ida_lines.tag_remove(x.print1(None)))):
+    """run query on list of addresses, print results"""
+
+    if not ea_list:
+        ea_list = [ida_kernwin.get_screen_ea()]
+
+    r = exec_query(query, ea_list)
+    try:
+        for e in r:
+            print(fmt(e))
+    except:
+        print("<query> error!")
+    return
+
+# ----------------------------------------------------------------------------
+def display(f,
+        fmt=lambda x:"%x: %s" % (x.ea,
+            ida_lines.tag_remove(x.print1(None)))):
+    """execute function f and print results according to fmt.
+
+    f is expected to return a list of cexpr_t objects
+    """
+
+    try:
+        for e in f():
+            print(fmt(e))
+    except Exception as exc:
+        print("<display> error!:", exc)
+    return
+
+# ----------------------------------------------------------------------------
+def display_argstr(f, idx):
+    """execute function f and print results.
+    
+    idx is an index into the argument list of a cexpr_t 
+    """
+
+    try:
+        display(f, lambda x:"%x: %s" % (x.ea,
+            ida_bytes.get_strlit_contents(x.a[idx].obj_ea, -1, 0,
+                ida_bytes.STRCONV_ESCAPE).decode("utf-8")))
+    except Exception as exc:
+        print("<display_argstr> error!:", exc)
+    return
