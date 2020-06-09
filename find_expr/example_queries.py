69e631e8a24634b8cae24de085f860b8103f4f6e
Dennis Elser <patois@users.noreply.github.com>
2020-06-09 09:27:23 +0200

    Add files via upload

--- find_expr/example_queries.py
+++ find_expr/example_queries.py
@@ -40,19 +40,52 @@ def find_gpa():
 
     gpa = get_name_ea_simple('GetProcAddress')
     ea_list = [f.start_ea for f in [get_func(xref.frm) for xref in XrefsTo(gpa, XREF_FAR)] if f]
-    return _exec_query(query, list(dict.fromkeys(ea_list)), lambda x:"%x: %s" % (x.ea,
-            get_strlit_contents(x.a[1].obj_ea, -1, 0, STRCONV_ESCAPE).decode("utf-8")))
+    return _exec_query(query, list(dict.fromkeys(ea_list)))
 
 # ----------------------------------------------------------------------------
-def _db_exec_query(query, fmt=lambda x:"%x: %s" % (x.ea, tag_remove(x.print1(None)))):
+def _db_exec_query(query):
+    """run query on all functions in current db"""
     result = []
     for ea in Functions():
-        result += [fmt(e) for e in find_expr(ea, query)]
+        result += [e for e in find_expr(ea, query)]
     return result
 
 # ----------------------------------------------------------------------------
-def _exec_query(query, ea_list, fmt=lambda x:"%x: %s" % (x.ea, tag_remove(x.print1(None)))):
+def _exec_query(query, ea_list):
+    """run query on list of addresses"""
     result = []
     for ea in ea_list:
-        result += [fmt(e) for e in find_expr(ea, query)]
+        result += [e for e in find_expr(ea, query)]
     return result
+
+# ----------------------------------------------------------------------------
+def d(f, fmt=lambda x:"%x: %s" % (x.ea, tag_remove(x.print1(None)))):
+    """execute f and display results according to fmt.
+    f is supposed to return a list of cexpr_t objects"""
+    try:
+        for e in f():
+            print(fmt(e))
+    except:
+        print("<d> error!")
+    return
+
+# ----------------------------------------------------------------------------
+def ds(f):
+    try:
+        d(f, lambda x:"%x: %s" % (x.ea,
+                get_strlit_contents(x.a[1].obj_ea, -1, 0, STRCONV_ESCAPE).decode("utf-8")))
+    except:
+        print("<ds> error!")
+    return
+
+# ----------------------------------------------------------------------------
+def m():
+    print("""Try running the following Python statements:
+
+d(find_memcpy)
+d(find_sprintf)
+ds(find_gpa)""")
+    return
+
+if __name__ == "__main__":
+    m()
